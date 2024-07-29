404edc008e94d6da8ba53b043dba3e5d5dd5f84d
vanessalagomachado <vanessalagomachado@gmail.com>
2024-07-29 17:13:37 -0300

    Aula dia 29-07: 	- Correção da atividade CalculaArea usando HTML  	- Introdução ao uso de formulário HTML para calcular área e perímetro usando dados informado nos campos HTML

--- public_html/js/area_retangulo.js
+++ public_html/js/area_retangulo.js
@@ -1,4 +1,29 @@
 function calcularArea(){
-    // Algoritmo para calcular área e perímetro do retângulo com base na base e altura
+    /* Algoritmo para calcular área e perímetro do retângulo 
+    com base na base e altura
+    */
     
+    var base = parseInt(prompt("Informe o valor da base"));
+    var altura = parseInt(prompt("Informe o valor da altura"));
+
+
+    var area = base * altura;
+    var perimetro = base + base + altura + altura;
+
+    alert("Área do Retângulo: "+area+
+        "\n Perímetro do Retângulo: "+perimetro);
+}
+
+function calcularAreaFormulario(){
+    var base = document.getElementById("numBase").value;
+    base = parseInt(base);
+    
+    var altura = parseInt(document.getElementById("numAltura").value);
+    
+    var area = base * altura;
+    var perimetro = base + base + altura + altura;
+
+    alert("Área do Retângulo: "+area+
+        "\n Perímetro do Retângulo: "+perimetro);
+
 }
